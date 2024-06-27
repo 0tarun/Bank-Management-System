dcb80c0dc9cdeed8973944c6fb817522b9e9ed9a
0tarun <tdas221498@bscse.uiu.ac.bd>
2024-06-28 02:37:36 +0600

    creat a new calss and change somthing

---
+++ Bank Management System/Card.java
@@ -0,0 +1,4 @@
+public class Card {
+    private String suit;
+    
+}
--- Bank Management System/Main.java
+++ Bank Management System/Main.java
@@ -1,6 +1,8 @@
 class Main {
     public static void main(String[] args) {
+
         Bank bc=new Bank();
+        System.out.println("Your code is start here ");
         bc.newAccount();
  }
 }
